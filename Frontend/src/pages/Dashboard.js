6de41044c28bc0bd949ff75634ba96ed0b05915f
Anushka2925 <157286081+Anushka2925@users.noreply.github.com>
2025-09-28 07:41:22 +0530

    Create Dashboard.js

---
+++ Frontend/src/pages/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Dashboard() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    alert('Logged out');
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+}
+
+export default Dashboard;
